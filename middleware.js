f7364ac0294ab4f333814e7fd3d902372cb0d1ee
yogesh191299 <86140601+yogesh191299@users.noreply.github.com>
2022-11-27 12:06:36 +0530

    Error Handling

--- middleware.js
+++ middleware.js
@@ -5,16 +5,26 @@ const app = express();
 
 function middleware1(req, res, next) {
     console.log("Yo!! I am middleware #1");
-    next();
+
+    const err = new Error("Yo I am error"); // creating an error for the sake of understanding that if there was en error how would it behave.
+    next(err);
+}
+
+function errorHandler(err, req, res, next) {
+    if(err) {
+        console.log("This is an error");
+    }
 }
 
 app.use(middleware1); // global middleware declaration
-                      // eg bodyParser passportJS ,etc.,
+                     // eg bodyParser passportJS ,etc.,
 
 function middleware2(req, res, next) {
     console.log("Yo!! I am middleware #2");
     next();
 }
+
+
 
 function mainMiddleware(req, res, next) {
     console.log("Yo!! I am Main middleware ");
@@ -22,6 +32,8 @@ function mainMiddleware(req, res, next) {
 }
 
 app.get('/', middleware2, mainMiddleware);
+
+app.use(errorHandler);     // we always place this at last since through entire middlewares calling it should redirect to last where error is handled and do not affect the functionining of other middlewares.
 
 ///////////local middleware declaration///////
 
